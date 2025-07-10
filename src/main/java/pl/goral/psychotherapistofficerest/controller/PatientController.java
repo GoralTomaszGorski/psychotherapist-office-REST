caed9a53bf2ea1d3e2cd1bb48232a722002d44bb
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-07-10 22:23:04 +0200

    Add endpoint to fetch all patients in PatientController

--- src/main/java/pl/goral/psychotherapistofficerest/controller/PatientController.java
+++ src/main/java/pl/goral/psychotherapistofficerest/controller/PatientController.java
@@ -1,4 +1,25 @@
 package pl.goral.psychotherapistofficerest.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.goral.psychotherapistofficerest.model.Patient;
+import pl.goral.psychotherapistofficerest.service.PatientService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class PatientController {
+
+    private final PatientService patientService;
+
+    @GetMapping("/patients")
+    public List<Patient> getPatient(){
+        return patientService.findAllPatients();
+    }
+
+
+
+
 }
