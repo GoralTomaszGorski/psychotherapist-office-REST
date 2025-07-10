fb2c3c42d0a218b5f0da344216964eb8000109aa
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-07-10 22:22:53 +0200

    Introduce `Patient` entity with JPA annotations

--- src/main/java/pl/goral/psychotherapistofficerest/model/Patient.java
+++ src/main/java/pl/goral/psychotherapistofficerest/model/Patient.java
@@ -1,4 +1,31 @@
 package pl.goral.psychotherapistofficerest.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "patient")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    private String nick;
+    private String name;
+    private String surname;
+    private String email;
+    private String telephone;
+    @Column(name = "YEAR_OF_BRITH")
+    private int yearOfBrith;
+    @Column(name = "JOIN_DATE")
+    private ZonedDateTime joinDate;
+    private String  information;
+    private boolean  approval;
+
 }
