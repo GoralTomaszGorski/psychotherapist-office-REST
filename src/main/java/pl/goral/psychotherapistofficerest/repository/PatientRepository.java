43ce6c3ae9a792ea40d8a723fa7c1a285691640d
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-07-10 22:22:58 +0200

    Add PatientService and PatientRepository with basic methods

--- src/main/java/pl/goral/psychotherapistofficerest/repository/PatientRepository.java
+++ src/main/java/pl/goral/psychotherapistofficerest/repository/PatientRepository.java
@@ -1,4 +1,18 @@
-package pl.goral.psychotherapistofficerest.service;
+package pl.goral.psychotherapistofficerest.repository;
 
-public class PatientRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.goral.psychotherapistofficerest.model.Patient;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    @Override
+    List<Patient> findAll();
+
+    Optional<Patient> findById(Long id);
+
+
 }
--- src/main/java/pl/goral/psychotherapistofficerest/service/PatientService.java
+++ src/main/java/pl/goral/psychotherapistofficerest/service/PatientService.java
@@ -1,4 +1,27 @@
-package pl.goral.psychotherapistofficerest.controller;
+package pl.goral.psychotherapistofficerest.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.goral.psychotherapistofficerest.model.Patient;
+import pl.goral.psychotherapistofficerest.repository.PatientRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+
+    public List<Patient> findAllPatients(){
+        return patientRepository.findAll();
+    }
+
+    public Optional<Patient> findPatientById(long id){
+        return patientRepository.findById(id);
+    }
+
+
 }
