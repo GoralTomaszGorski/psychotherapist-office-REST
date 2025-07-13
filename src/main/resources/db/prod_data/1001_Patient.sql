54df67849192713a2d54fc3ff70880c71c8215db
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-07-13 20:16:59 +0200

    test data add to db

---
+++ src/main/resources/db/prod_data/1001_Patient.sql
@@ -0,0 +1,5 @@
+insert into patient
+(nick, name, surname, telephone, year_of_brith, JOIN_DATE, email, information, approval)
+values
+
+    ('215TGo','Tomasz','Górski','603215114',	1988	,null,'tomasz.gorski88@gmail.com','ul. Mickiewicza 4/29, 23-204 Kraśnik',	true	)	;
