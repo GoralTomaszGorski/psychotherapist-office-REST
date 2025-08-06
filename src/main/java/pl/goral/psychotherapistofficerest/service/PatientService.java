85dc7ffb33129b383238de5b282568ffbd2da5fa
Goral <tomasz.gorski88@gmail.com>
2025-08-06 21:09:13 +0200

    "Inject DateInWarsaw dependency into PatientService"

--- src/main/java/pl/goral/psychotherapistofficerest/service/PatientService.java
+++ src/main/java/pl/goral/psychotherapistofficerest/service/PatientService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.goral.psychotherapistofficerest.model.Patient;
 import pl.goral.psychotherapistofficerest.repository.PatientRepository;
+import pl.goral.psychotherapistofficerest.utils.DateInWarsaw;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,7 @@ import java.util.Optional;
 public class PatientService {
 
     private final PatientRepository patientRepository;
+    private final DateInWarsaw dateInWarsaw;
 
     public List<Patient> findAllPatients() {
         return patientRepository.findAll();
