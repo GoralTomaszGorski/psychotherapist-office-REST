fd37e914adb353903c1fe85473bb46a624c7ed11
Goral <tomasz.gorski88@gmail.com>
2025-08-21 21:54:29 +0200

    "Introduce UserDto class to encapsulate user details, supporting role-based access control and improving data transfer consistency."

---
+++ src/main/java/pl/goral/psychotherapistofficerest/dto/UserDto.java
@@ -0,0 +1,18 @@
+package pl.goral.psychotherapistofficerest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String email;
+    private Set<String> roles;
+}
