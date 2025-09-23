acbd1573092484f2fd51c32575f46dba1070b724
Goral <tomasz.gorski88@gmail.com>
2025-09-23 21:16:31 +0200

    "Rename `Calender` to `CalenderSlot` and update references in `Appointment` model for naming consistency and clarity"

--- src/main/java/pl/goral/psychotherapistofficerest/model/CalenderSlot.java
+++ src/main/java/pl/goral/psychotherapistofficerest/model/CalenderSlot.java
@@ -7,14 +7,16 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "calender")
 
-public class Calender {
+public class CalenderSlot {
     @Id
     @Column(name = "id")
     private Long id;
