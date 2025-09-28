97535533c5c28125a0eaff098095c1643968ccf9
Goral <tomasz.gorski88@gmail.com>
2025-09-28 10:59:22 +0200

    "Add unit tests for `EmailMessageService` and update example Gmail credentials in `config.properties`"

---
+++ src/test/java/pl/goral/psychotherapistofficerest/service/EmailMessageServiceTest.java
@@ -0,0 +1,16 @@
+package pl.goral.psychotherapistofficerest.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailMessageServiceTest {
+
+    @Test
+    void sendResetPasswordMail() {
+    }
+
+    @Test
+    void sendVisitReminderMail() {
+    }
+}
