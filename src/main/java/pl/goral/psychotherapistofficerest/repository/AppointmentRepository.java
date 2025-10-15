8bf25afe3bef500668e3bae25b2c3828ac20754a
Goral <tomasz.gorski88@gmail.com>
2025-10-15 23:21:29 +0200

    Add `searchAppointmentsByKeyword` method in `AppointmentRepository` for flexible keyword-based search

--- src/main/java/pl/goral/psychotherapistofficerest/repository/AppointmentRepository.java
+++ src/main/java/pl/goral/psychotherapistofficerest/repository/AppointmentRepository.java
@@ -1,5 +1,7 @@
 package pl.goral.psychotherapistofficerest.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.goral.psychotherapistofficerest.model.Appointment;
 
 import java.util.List;
@@ -8,4 +10,20 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
     List<Appointment> findAllByCalenderSlotIsNotNullOrderByCalenderSlot();
     List<Appointment> findAllByPatient_Id(Long patientId);
     List<Appointment> findByStatus(String status);
+    @Query("""
+    SELECT a FROM Appointment a
+    JOIN a.patient p
+    JOIN a.calenderSlot c
+    WHERE
+        LOWER(p.nick) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        OR LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        OR LOWER(p.surname) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        OR LOWER(p.email) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        OR LOWER(p.telephone) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        OR STR(p.yearOfBrith) LIKE CONCAT('%', :keyword, '%')
+        OR LOWER(c.dayOfWeek) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        OR STR(c.date) LIKE CONCAT('%', :keyword, '%')
+        OR STR(c.time) LIKE CONCAT('%', :keyword, '%')
+""")
+    List<Appointment> searchAppointmentsByKeyword(@Param("keyword") String keyword);
 }
